afb1a403fcf01a50173ad06912a7cd71d36d8052
fernando <fer20comp@gmail.com>
2023-05-25 20:30:00 -0300

    formating mock data

--- front_mind/src/components/ProductListView.tsx
+++ front_mind/src/components/ProductListView.tsx
@@ -1,30 +1,87 @@
-import ProductView from './ProductView';
-import "../style/ProductListView.css"
-import Product from '../interfaces/Product';
+import ProductView from "./ProductView";
+import "../style/ProductListView.css";
+import Product from "../interfaces/Product";
 
 function ProductListView() {
   const numbers = [1, 2, 3, 4, 5, 6, 6, 7];
-  const products: Product[] =  [{ id: "1", name: "Cadeira", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" }, 
-                                { id: "2", name: "Mesa", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" }, 
-                                { id: "3", name: "Sofá", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "4", name: "Cinzeiro", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "5", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "6", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "7", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "8", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "9", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "10", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" },
-                                { id: "11", name: "Tv", email: "@@@@@@gmail.com", description: "aaaaaaaaaaa" }]
+  const products: Product[] = [
+    {
+      id: "1",
+      name: "Cadeira",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "2",
+      name: "Mesa",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "3",
+      name: "Sofá",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "4",
+      name: "Cinzeiro",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "5",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "6",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "7",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "8",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "9",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "10",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+    {
+      id: "11",
+      name: "Tv",
+      email: "@@@@@@gmail.com",
+      description: "aaaaaaaaaaa",
+    },
+  ];
 
-  const listItems = products.map((product) =>
+  const listItems = products.map((product) => (
     <li key={product.id}>{ProductView(product)}</li>
-  );
+  ));
 
   return (
     <div>
-    <h1> Lista de produtos </h1>
-    <ul className="ProductList"> {listItems} </ul>
+      <h1> Lista de produtos </h1>
+      <ul className="ProductList"> {listItems} </ul>
     </div>
   );
 }
-export default ProductListView
+export default ProductListView;
--- front_mind/src/components/ProductView.tsx
+++ front_mind/src/components/ProductView.tsx
@@ -1,19 +1,19 @@
-import "../style/ProductView.css"
-import Product from "../interfaces/Product"
+import "../style/ProductView.css";
+import Product from "../interfaces/Product";
 
 const ProductView = (product: Product) => {
-    const buttonHandler = () => {
-        console.log("button pressed")
-    }
+  const buttonHandler = () => {
+    console.log("button pressed");
+  };
 
-    return (
-        <div className="ProductCard">
-            <h1> {product.name} </h1>    
-            <p> {product.email} </p> 
-            <p> {product.description} </p>
-            <button onClick={buttonHandler} >See details </button>
-        </div>
-    )
-}
+  return (
+    <div className="ProductCard">
+      <h1> {product.name} </h1>
+      <p> {product.email} </p>
+      <p> {product.description} </p>
+      <button onClick={buttonHandler}>See details </button>
+    </div>
+  );
+};
 
-export default ProductView
+export default ProductView;
