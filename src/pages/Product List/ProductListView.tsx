41439508186922bdddea27348c82a6abbc257e0f
fernando <fer20comp@gmail.com>
2023-06-29 16:10:05 -0300

    search bar

--- src/pages/Product List/ProductListView.tsx
+++ src/pages/Product List/ProductListView.tsx
@@ -4,20 +4,41 @@ import Product from "../../interfaces/Product";
 import { Firebase } from "../../services/firebase";
 import { useState, useEffect } from "react";
 
+const getFilteredItems = (query: string, items: Product[]) => {
+  query.toLowerCase()
+  if (!query) {
+    return items;
+  }
+  return items.filter(
+    (product) =>
+      product.name.toLowerCase().includes(query) ||
+      product.description.toLowerCase().includes(query) ||
+      product.email.toLowerCase().includes(query)
+  );
+};
+
 function ProductListView() {
+  const [query, setQuery] = useState<string>("");
   const [minhaLista, setMinhaLista] = useState<Product[]>([]);
+  const filteredItems = getFilteredItems(query, minhaLista);
 
   useEffect(() => {
-    Firebase.getProducts().then((productList) => setMinhaLista(productList))
-  }, [])
-  
-  const listItems = minhaLista.map((product) => (
-    <li className="ListView" key={product.id.toString()}>{ProductView(product)}</li>
+    Firebase.getProducts().then((productList) => setMinhaLista(productList));
+  }, []);
+
+  const listItems = filteredItems.map((product) => (
+    <li className="ListView" key={product.id.toString()}>
+      {ProductView(product)}
+    </li>
   ));
 
   return (
     <div className="Page">
       <h1 className="PageTitle">Product's List</h1>
+      <div className="SearchBar">
+        <label>Search</label>
+        <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      </div>
       <ul className="ProductList"> {listItems} </ul>
     </div>
   );
