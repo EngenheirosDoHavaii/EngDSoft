ba1a68271aac9243582920837fedf1ca4aec69dd
Luis Felipe <l182956@dac.unicamp.br>
2023-06-29 00:58:12 -0300

    Fix issue on page reload

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,19 +1,23 @@
 import { Firebase } from "../../services/firebase";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AddItemModal from "../../components/Modal/AddItemModal";
-import profilePic from "../../images/profile-pic-mock.jpg"
-import "./Profile.css"
+import profilePic from "../../images/profile-pic-mock.jpg";
+import "./Profile.css";
+import { User } from "firebase/auth";
 
 export function Profile() {
   const navigate = useNavigate();
   const auth = Firebase.GetAuth();
+  const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (!user) {
         console.log("User did log out");
         navigate("/signin");
+      } else {
+        setUserData(user);
       }
     });
     return () => {
@@ -21,28 +25,36 @@ export function Profile() {
     };
   }, [auth, navigate]);
 
+  console.log(userData);
   return (
     <div className="container-profile">
       <div className="profile-content">
-
         <div className="profile-header">
-
           <div className="top-header">
-            <img src={profilePic} alt="user profile" className="profile-pic"></img>
-            <button type="button" onClick={Firebase.LogOut} className="logout-button">
+            <img
+              src={profilePic}
+              alt="user profile"
+              className="profile-pic"
+            ></img>
+            <button
+              type="button"
+              onClick={Firebase.LogOut}
+              className="logout-button"
+            >
               Logout
             </button>
           </div>
-          
-          <div className="user-profile-text">
-            <div className="user-name">{auth.currentUser?.displayName}</div>
-            <div className="user-date"> Since day, month year</div>
-            <div className="user-email">{auth.currentUser?.email}</div>
-          </div>
 
+          {userData && (
+            <div className="user-profile-text">
+              <div className="user-name">{userData.displayName}</div>
+              <div className="user-date">Since day, month year</div>
+              <div className="user-email">{userData.email}</div>
+            </div>
+          )}
         </div>
 
-        <hr></hr>
+        <hr />
 
         <div className="profile-body">
           <h4 className="user-items-title">Your products:</h4>
@@ -50,7 +62,6 @@ export function Profile() {
             <AddItemModal />
           </div>
         </div>
-
       </div>
     </div>
   );
